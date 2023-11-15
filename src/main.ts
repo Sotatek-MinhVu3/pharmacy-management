456668d7a55b176458ff49b578832a471d7e90d0
Sotatek-MinhVu3 <minh.vu3@sotatek.com>
2023-11-15 18:37:15 +0700

    add port v2

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3000;
-  await app.listen(3000, process.env.APP_HOST);
+  await app.listen(port, process.env.APP_HOST);
 }
 bootstrap();
