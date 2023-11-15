7ea515d51842b3a948dde520202dceb7dea84871
Sotatek-MinhVu3 <minh.vu3@sotatek.com>
2023-11-15 18:34:29 +0700

    add port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const port = process.env.PORT || 3000;
   await app.listen(3000, process.env.APP_HOST);
 }
 bootstrap();
